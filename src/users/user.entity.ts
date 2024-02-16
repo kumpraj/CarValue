78e1e1b828942ab71e3d3e2511bd757832965011
Prajwal Kumar <kumpraj@gmail.com>
2024-02-16 20:42:34 +0530

    added other methods in user services for CRUD operation

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,5 +10,22 @@ export class User {
 
     @Column()
     password: string;
+    
+    @AfterInsert()
+    logInsert(){
+        console.log('Inserted User with id: ', this.id);
+    }
+
+    @AfterUpdate()
+    logUpdate(){
+        console
+        .log('Updated User with id: ', this.id);
+    }
+
+    @AfterRemove()
+    logRemove(){
+        console.log('Removed User with id: ', this.id);
+    }
+
 
 }
