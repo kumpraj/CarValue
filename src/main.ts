fccd310f69a998dd265d3afe5c4ba343e468c0f4
Prajwal Kumar <kumpraj@gmail.com>
2024-03-27 17:21:42 +0530

    e2e or integration testing for auth

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session'); // doing this because of config mismatch between nest and cookie-session
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.use(
-    cookieSession({
-      keys: ['asdfasdf'],
-    }),
-  );
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  const app = await NestFactory.create(AppModule);  
 
   await app.listen(3000);
 }
